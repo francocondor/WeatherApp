e0f8ac4d5cab408b3642f91a1bf9c34578e2a75d
francobko <franco.condor.urp@gmail.com>
2019-01-27 19:44:36 -0500

    proptypes shape

--- weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import './styles.css';
+import PropTypes from 'prop-types';
 
 const WeatherData = ({data : { temperature, weatherState, humidity, wind }}) => (
     <div className="weatherDataCont" >
@@ -9,5 +10,14 @@ const WeatherData = ({data : { temperature, weatherState, humidity, wind }}) => 
         <WeatherExtraInfo humidity={humidity} wind={wind}></WeatherExtraInfo>
     </div>
 );
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.number.isRequired
+    })
+};
 
 export default WeatherData;
