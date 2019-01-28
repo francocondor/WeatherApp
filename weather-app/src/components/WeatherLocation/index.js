5816f2422ec859ce5586573873f20f00fa6a755a
francobko <franco.condor.urp@gmail.com>
2019-01-27 22:04:51 -0500

    fetch weathermap api

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -6,6 +6,12 @@ import {
     SUN
 } from '../../constants/Weathers';
 
+const location = "Barranco, PE";
+const api_key = "b4f34322ad75d46b137a6e3d9d0aa468";
+const url_base_weather = "https://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 const data = {
     temperature: 5,
     weatherState: SUN,
@@ -25,6 +31,7 @@ class WeatherLocation extends Component {
 
     handleUpdateClick = () => {
         console.log("Actualizado");
+        fetch(api_weather);
         this.setState({
             city:"Miraflores"
         });
