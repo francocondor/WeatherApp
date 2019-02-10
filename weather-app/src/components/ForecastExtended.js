1accc97da0a8767fa2e17dd0b3ac0c77467bb7e0
francobko <franco.condor.urp@gmail.com>
2019-02-09 23:13:05 -0500

    componentWillReceiveProps

--- weather-app/src/components/ForecastExtended.js
+++ weather-app/src/components/ForecastExtended.js
@@ -28,7 +28,18 @@ class ForecastExtended extends Component {
     }
 
     componentDidMount() {
-        const url_forecast = `${url_base_weather}?q=${this.props.city}&appid=${api_key}`;
+        this.updateCity(this.props.city);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.city !== this.props.city) {
+            this.setState({ forecastData: null });
+            this.updateCity(nextProps.city);
+        }
+    }
+
+    updateCity = city => {
+        const url_forecast = `${url_base_weather}?q=${city}&appid=${api_key}`;
         fetch(url_forecast).then(
             data => (data.json())
         ).then(
@@ -42,12 +53,12 @@ class ForecastExtended extends Component {
     }
 
     renderForecastItemDays(forecastData) {
-        return forecastData.map (
+        return forecastData.map(
             forecast => (
-                <ForecastItem 
+                <ForecastItem
                     key={`${forecast.weekDay}${forecast.hour}`}
-                    weekDay={forecast.weekDay} 
-                    hour={forecast.hour} 
+                    weekDay={forecast.weekDay}
+                    hour={forecast.hour}
                     data={forecast.data}>
                 </ForecastItem>
             )
